2279baefb3f6a482ecd7e8ea79eabf05acd6d9e2
SANJITH A <sanjith.a@HO-002-LP-01322.local>
2024-08-18 16:55:13 +0530

    solved medium of 01

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,25 @@
 */
 
 function isPalindrome(str) {
-  return true;
+  str=str.toLowerCase();
+  const chars= new Array();
+  for (let index = 0; index < str.length; index++) {
+    let char=str[index].charCodeAt(0);
+    if(char>=97 && char<=122){
+      chars.push(char);
+    }
+    
+  }
+  let i=0,j=chars.length-1;
+  while(i<=j){
+    if(chars[i]!=chars[j]){
+      return false;
+    }
+    i++;
+    j--;
+  }
+
+    return true;
 }
 
 module.exports = isPalindrome;
