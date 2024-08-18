1110f48624d94349e9b81ba1d6282be982e49e02
SANJITH A <sanjith.a@HO-002-LP-01322.local>
2024-08-18 15:50:16 +0530

    easy1

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,34 @@
 */
 
 function isAnagram(str1, str2) {
+    if(str1.length!=str2.length)return false;
+    str1=str1.toLowerCase();
+    str2=str2.toLowerCase();
+    const count =new Array(26);
+    count.fill(0);
 
+    for(let i=0;i<str1.length;i++){
+      let char =str1[i];
+      let unichar=char.charCodeAt(0);
+      let a_uni='a'.charCodeAt(0);
+      count[unichar-a_uni]++;
+
+
+    }
+    for(let i=0;i<str2.length;i++){
+      let char =str2[i];
+      let unichar=char.charCodeAt(0);
+      let a_uni='a'.charCodeAt(0);
+      count[unichar-a_uni]--;
+    }
+
+    for (let i = 0; i < 26; i++) {
+      if(count[i]!==0){
+        return false;
+      }
+      
+    }
+    return true;
 }
 
 module.exports = isAnagram;
